02ecd940cf411401956caa90ab396abf8e0d32b3
Olga P <olga.p@alyce.com>
2022-04-20 14:00:16 +0300

    fix after review

--- hw1-javascript-fun.js
+++ hw1-javascript-fun.js
@@ -13,7 +13,7 @@ function age(age_1, age_2, age_3) {
        }
 
  }
- console.log(age ( 17, 18, 612))
+ const checkAge =  age(17, 18, 612);
 
 
 //Задание 2*: Преобразовать 2* таким образом, чтобы значение '2' (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number4***:
@@ -35,7 +35,7 @@ function age(age_1, age_2, age_3) {
        }
 
 }
- console.log(age2 (12,  "fd", 612))
+const checkAge2 =  age2(12,  "fd", 612);
 
 // Задание 3*: Преобразовать 2* таким образом, чтобы значение '2' (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number4***:
 // Комментарий к решению: функция из задания 2 выполняет требования задания 3
@@ -58,13 +58,13 @@ function age3(age_1, age_2, age_3) {
           }
 
 }
-console.log(age3 ("12", 23, 612))
+const checkAge3 = age3("12", 23, 612);
 
 
 // Задание 4*: Преобразовать задание 3* таким образом, чтобы возраст вводится используя функцию prompt в привязанной верстке
 
 
-function age3(age_1, age_2, age_3) {
+function age4(age_1, age_2, age_3) {
   if (/^(0|[1-9]\d*)$/.test(age_1) && /^(0|[1-9]\d*)$/.test(age_2)  && /^(0|[1-9]\d*)$/.test(age_3) ) {
         if (age_1 < age_2) {
             return (`"You don\'t have access cause your age is "${age_1}" It\'s less then"`);
@@ -85,5 +85,5 @@ function age3(age_1, age_2, age_3) {
 }
 
 let age_1 = prompt('Could you please fill in your age');
-
-console.log(age3 ( age_1, 23, 612))
+ 
+ console.log(age4(age_1, 23, 612))
